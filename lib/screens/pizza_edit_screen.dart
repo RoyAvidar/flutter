06de7ff1350ad91908368ba70df71b22a7a954cc
Roy Avidar <Roi981av@gmail.com>
2021-01-21 14:52:33 +0200

    added Edit Screen

---
+++ lib/screens/pizza_edit_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PizzaEditScreen extends StatefulWidget {
+  @override
+  _PizzaEditScreenState createState() => _PizzaEditScreenState();
+}
+
+class _PizzaEditScreenState extends State<PizzaEditScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
