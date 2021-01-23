fef95cf833889537adfe36dcaf2efedae3c484de
Damrir <roi981av@gmail.com>
2021-01-23 15:14:18 +0200

    elad function

--- lib/providers/edit_topping.dart
+++ lib/providers/edit_topping.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 import '../models/topping.dart';
 
 class EditTopping with ChangeNotifier {
-  void elad() {}
+  void elad() {
+    print(Text('elad'));
+  }
 }
