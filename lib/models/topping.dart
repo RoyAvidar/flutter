40b30291b0812c2cf09954e33d21a7bcf5ec4eb2
Roy Avidar <Roi981av@gmail.com>
2021-01-25 16:09:56 +0200

    changed Topping structure

--- lib/models/topping.dart
+++ lib/models/topping.dart
@@ -1,14 +1,16 @@
-enum Topping {
-  pepperoni,
-  basil,
-  onion,
-  mushroom,
-  black_olives,
-  green_olives,
-  pineapple,
-  bacon,
-  sweetcorn,
-  artichoke,
-  margarita,
-  plain,
+import 'package:flutter/material.dart';
+
+class Topping {
+  String name;
+  int price;
+
+  Topping(this.name, this.price);
+}
+
+class Toppings extends ChangeNotifier {
+  final peperoni = Topping('peperoni', 1);
+  final bazil = Topping('peperoni', 1);
+  final mushroom = Topping('peperoni', 1);
+  final onion = Topping('peperoni', 1);
+  final margherita = Topping('peperoni', 1);
 }
