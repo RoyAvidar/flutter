ecbebb28437510dc9c7c35c64fbe32d9c1e96dac
Roy Avidar <Roi981av@gmail.com>
2021-02-03 15:18:44 +0200

    added TextFormField to the form

--- lib/screens/edit_address_screen.dart
+++ lib/screens/edit_address_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_pizza/providers/address.dart';
+import '../models/address.dart';
 
 class EditAddressScreen extends StatefulWidget {
   static const routeName = '/edit-address-screen';
@@ -7,6 +9,12 @@ class EditAddressScreen extends StatefulWidget {
 }
 
 class _EditAddressScreenState extends State<EditAddressScreen> {
+  var _editedAddress = AddressItem(
+    addressId: null,
+    cityName: '',
+    streetName: '',
+    streetNumber: 0,
+  );
   final _formKey = GlobalKey<FormState>();
   var _isLoading = false;
   @override
@@ -30,7 +38,63 @@ class _EditAddressScreenState extends State<EditAddressScreen> {
               child: Form(
                 key: _formKey,
                 child: ListView(
-                  children: <Widget>[TextFormField()],
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'City Name'),
+                      textInputAction: TextInputAction.next,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please provide a value';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _editedAddress = AddressItem(
+                          addressId: _editedAddress.addressId,
+                          cityName: value,
+                          streetName: _editedAddress.streetName,
+                          streetNumber: _editedAddress.streetNumber,
+                        );
+                      },
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Street Name'),
+                      textInputAction: TextInputAction.next,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please provide a value';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _editedAddress = AddressItem(
+                          addressId: _editedAddress.addressId,
+                          cityName: _editedAddress.cityName,
+                          streetName: value,
+                          streetNumber: _editedAddress.streetNumber,
+                        );
+                      },
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(labelText: 'Street Number'),
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.number,
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please provide a value';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _editedAddress = AddressItem(
+                          addressId: _editedAddress.addressId,
+                          cityName: _editedAddress.cityName,
+                          streetName: _editedAddress.streetName,
+                          streetNumber: int.parse(value),
+                        );
+                      },
+                    ),
+                  ],
                 ),
               ),
             ),
